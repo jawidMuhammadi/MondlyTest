9ad78cad8644c54c83d50f6afcfdfd5c06f41a0e
Jawid Muhammadi <gitlab-ci@ocado.com>
2024-03-03 21:26:32 +0100

    Add unit tests for GetProductsUseCaseTest

---
+++ core/domain/src/test/java/com/jawidmuhammadi/domain/GetProductsUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.jawidmuhammadi.domain
+
+import com.jawidmuhammadi.data.ProductRepository
+import com.jawidmuhammadi.sharedtest.ProductItemMother
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class GetProductsUseCaseTest {
+
+    private val productRepository: ProductRepository = mockk {
+        coEvery { getProducts() } returns ProductItemMother.buildProductItems(3)
+    }
+
+    @Test
+    fun `given productRepository, when getProductsUseCase called, then return a list of products`() = runTest{
+        val getProducts = GetProductsUseCase(productRepository)
+
+        val actualResult = getProducts.invoke()
+        val expected = ProductItemMother.buildProductItems(3)
+
+        assertEquals(actualResult, expected)
+    }
+}
